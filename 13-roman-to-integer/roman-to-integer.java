4c2ba64f307e797549801bb94a589419974f4fd4
Swetha <swethasweety0814@gmail.com>
2025-10-28 20:58:02 +0530

    Time: 2 ms (100.00%) | Memory: 44.8 MB (70.80%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,31 @@
+
+class Solution {
+    private int value(char r) {
+        switch (r) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+    public int romanToInt(String s) {
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = value(s.charAt(i));
+
+            if (currentValue < prevValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+            prevValue = currentValue;
+        }
+        return result;
+    }
+}
